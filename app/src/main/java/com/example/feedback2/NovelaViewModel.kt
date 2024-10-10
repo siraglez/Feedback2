df4dfdf280b5fc137a124b2d48954a46930ff37d
Sira <sgonzmad@myuax.com>
2024-10-10 19:48:57 +0200

    modificar NovelaViewModel para implementar Firebase

--- app/src/main/java/com/example/feedback2/NovelaViewModel.kt
+++ app/src/main/java/com/example/feedback2/NovelaViewModel.kt
@@ -2,12 +2,33 @@ package com.example.feedback2
 
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.tasks.await
 
 class NovelaViewModel : ViewModel() {
 
     //Lista de novelas en memoria
     private val _novelas = mutableStateListOf<Novela>()
     val novelas: List<Novela> get() = _novelas
+
+    private val db: FirebaseFirestore = Firebase.firestore
+
+    //Función para cargar novelas desde Firebase
+    suspend fun cargarNovelas() {
+        try {
+            val documentos = db.collection("Novelas").get().await()
+            _novelas.clear() //Limpiar la lista antes de cargar nuevas novelas
+            for (documento in documentos) {
+                val novela = documento.toObject(Novela::class.java)
+                _novelas.add(novela)
+            }
+        } catch (e: Exception) {
+            //Manejo de errores
+            e.printStackTrace()
+        }
+    }
 
     fun agregarNovela(novela: Novela) {
         _novelas.add(novela)
